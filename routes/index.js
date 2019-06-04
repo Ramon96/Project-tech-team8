9a6fd38900bd06bf0cc8c4c29daedb534c463fb3
Ramon96 <rfm1@live.nl>
2019-06-04 11:09:33 +0200

    next challange, the dot env file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var multer = require('multer');
 var path = require('path');
 var mongoose = require('mongoose');
+require('dotenv').config({path: __dirname + '/.env'}); //???
 
 mongoose.connect('mongodb://localhost:27018/test', {useNewUrlParser: true});
 var Schema = mongoose.Schema;
@@ -28,34 +29,8 @@ var userDataSchema = new Schema({
 
 var userData = mongoose.model('UserData', userDataSchema);
 
-var profileSettings = {
-  username: "John",
-  profilePicture: "images/capricorn.png",
-  premium: false,
-  aboutme: "I like turtles",
-  function: "Teacher",
-  company: "Good Students School",
-  city: "London",
-  isMale: false,
-  prefMen: true,
-  email: 'John_wick@gmail.com',
-  DoB: "1990-10-25",
-  minAge: 18,
-  maxAge: 25,
-  maxDistance: 10,
-  pushNotification: false
-};
-
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // res.render('index', {
-  //   title: 'Edit Profile',
-  //   Username: profileSettings.username,
-  //   UserProfile: profileSettings.profilePicture,
-  //   premium: profileSettings.premium
-  // });
-
   userData.findById(id)
   .then(function(doc){
     res.render('index', {
@@ -114,7 +89,6 @@ router.post('/upload', function (req, res) {
           UserProfile: `uploads/${req.file.filename}`
         });
       }
-      // profileSettings.profilePicture = `uploads/${req.file.filename}`;
       userData.findById(id, function(err, doc){
         if(err){
           console.error('entry not found');
@@ -127,14 +101,6 @@ router.post('/upload', function (req, res) {
 });
 
 router.get('/editprofile', function (req, res, next) {
-  // res.render('editprofile', {
-  //   title: 'Edit Profile',
-  //   aboutme: profileSettings.aboutme,
-  //   function: profileSettings.function,
-  //   company: profileSettings.company,
-  //   city: profileSettings.city,
-  //   isMale: profileSettings.isMale
-  // });
   userData.findById(id)
   .then(function(doc){
     res.render('editprofile', {
@@ -148,43 +114,6 @@ router.get('/editprofile', function (req, res, next) {
 
 
 });
-
-// router.get('/editprofile/get-data', function (req, res, next) {
-//   userData.findById(id)
-//     .then(function(doc){
-//       res.render('editprofile', {
-//         aboutme: doc.aboutme,
-//         function: doc.function,
-//         company: doc.company,
-//         city: doc.city,
-//         isMale: doc.isMale
-//       });
-//     });
-//     //Waarschrijnlijk wil ik hier Redirecten maar weet nog niet zo goed hoe ik dat doe zonder mn content te verliezen.
-// });
-
-// router.post('/editprofile/insert', function (req, res, next) {
-//   var item = {
-//     username: profileSettings.username,
-//     profilePicture: profileSettings.profilePicture,
-//     premium: profileSettings.premium,
-//     aboutme: profileSettings.aboutme,
-//     function: profileSettings.function,
-//     company: profileSettings.company,
-//     city: profileSettings.city,
-//     isMale: profileSettings.isMale,
-//     prefMen: profileSettings.prefMen,
-//     email: profileSettings.email,
-//     DoB: profileSettings.DoB,
-//     minAge: profileSettings.minAge,
-//     maxAge: profileSettings.maxAge,
-//     maxDistance: profileSettings.maxDistance
-//   };
-
-//   var data = new userData(item);
-//   data.save();
-//   res.redirect('/editprofile');
-// });
 
 router.post('/editprofile/update', function (req, res, next) {
   userData.findById(id, function(err, doc){
@@ -204,26 +133,10 @@ router.post('/editprofile/update', function (req, res, next) {
     doc.save();
   });
   res.redirect('/editprofile');
-
 });
 
-// router.post('/editprofile/delete', function (req, res, next) {
-
-// });
 
 router.get('/settings', function (req, res) {
-  // res.render('settings', {
-  //   title: 'More settings',
-  //   email: profileSettings.email,
-  //   username: profileSettings.username,
-  //   dob: profileSettings.DoB,
-  //   minAge: profileSettings.minAge,
-  //   maxAge: profileSettings.maxAge,
-  //   maxDistance: profileSettings.maxDistance,
-  //   pushNotification: profileSettings.pushNotification,
-  //   prefMen: profileSettings.prefMen
-  // });
-
   userData.findById(id)
   .then(function(doc){
     res.render('settings', {
@@ -237,7 +150,6 @@ router.get('/settings', function (req, res) {
       pushNotification: doc.pushNotification
     });
   });
-
 });
 
 router.post('/settings/update', function (req, res) {
@@ -268,4 +180,72 @@ router.post('/settings/update', function (req, res) {
 
   res.redirect('/settings')
 });
+
+router.get('/testarea', function(req, res){
+  res.render('testarea');
+});
+
+router.get('/testarea/get-data', function(req, res){
+  userData.find()
+    .then(function(doc){
+      res.render('testarea', {items: doc});
+    });
+});
+
+  router.post('/testarea/insert', function(req, res){
+      var item = {
+        username: req.body.username,
+        profilePicture: req.body.profilePicture,
+        premium: req.body.premium,
+        pushNotification: req.body.pushNotification,
+        aboutme: req.body.aboutme,
+        function: req.body.function,
+        company:req.body.company,
+        city: req.body.city,
+        email: req.body.email,
+        DoB: req.body.DoB,
+        maxDistance: req.body.maxDistance,
+        minAge: req.body.minAge,
+        maxAge: req.body.maxAge,
+        isMale: req.body.isMale,
+        prefMen: req.body.prefMen
+      }
+      var data = new userData(item);
+      data.save();
+      res.redirect('/testarea');
+  });
+
+  router.post('/testarea/update', function(req, res){
+    var chosenId = req.body.chosenId;
+    userData.findById(chosenId, function(err, doc){
+      if (err){
+        console.error('entry not found');
+      }
+      doc.username = req.body.username;
+      doc.profilePicture = req.body.profilePicture;
+      doc.premium = req.body.premium;
+      doc.pushNotification = req.body.pushNotification;
+      doc.aboutme = req.body.aboutme;
+      doc.function = req.body.function;
+      doc.company = req.body.company;
+      doc.city = req.body.city;
+      doc.email = req.body.email;
+      doc.DoB = req.body.DoB;
+      doc.maxDistance = req.body.maxDistance;
+      doc.minAge = req.body.minAge;
+      doc.maxAge = req.body.maxAge;
+      doc.isMale = req.body.isMale;
+      doc.prefMen = req.body.prefMen;
+    doc.save();
+    });
+    res.redirect('/testarea');
+
+  });
+  
+  router.post('/testarea/delete', function(req, res){
+    var chosenId = req.body.chosenId;
+    userData.findByIdAndRemove(chosenId).exec();
+    res.redirect('/testarea');
+  });
+
 module.exports = router;
